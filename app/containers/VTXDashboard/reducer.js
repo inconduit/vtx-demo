3fc8e7b27625f624b95a37fe3b20c8477fac5fc7
inconduit <adam@inconduit.com>
2018-07-05 11:32:15 -0400

    style(VTXDashboard): cleaner switch block

--- app/containers/VTXDashboard/reducer.js
+++ app/containers/VTXDashboard/reducer.js
@@ -18,14 +18,10 @@ function dashboardReducer(state = initialState, action) {
     case LOAD_ACCOUNT_SUCCESS:
       return state
         .set('account', action.account);
-    break;
-
     case LOAD_TOKEN_BALANCE_SUCCESS:
       return state
         .set('tokenBalance', action.tokenBalance)
         .set('tokenDecimals', action.tokenDecimals)
-     break;
-
      case TRANSFER_SUCCESS:
       const { transactionLog } = action
       const amount = transactionLog.args.value
@@ -35,8 +31,6 @@ function dashboardReducer(state = initialState, action) {
         .set('tokenBalance', newBalance)
         .update('transactionLogs',
           transactionLogs => transactionLogs.push(transactionLog))
-        break;
-
     default:
       return state;
   }
