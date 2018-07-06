4525094a07a6360a63c91945537a947bff499901
inconduit <adam@inconduit.com>
2018-07-05 22:28:01 -0400

    feat(VTXTransactionList): show transactions, add styling

--- app/components/VTXDashboard/VTXTransactionList/VTXTransactionList.js
+++ app/components/VTXDashboard/VTXTransactionList/VTXTransactionList.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { array } from 'prop-types';
 import List from 'components/List';
 import VTXTransactionListItem from 'components/VTXDashboard/VTXTransactionListItem';
+import './styles.scss'
 
 const VTXTransactionList = (props) => (
-  <List items={props.items} component={VTXTransactionListItem} />
+  <div className="vtx-transaction-list">
+    <h2 className="header">
+      Transaction List
+    </h2>
+
+    {props.items && props.items.length > 0
+      ? <List
+          items={props.items}
+          component={VTXTransactionListItem} />
+      : <h4>No transactions yet</h4>}
+  </div>
 );
 
 VTXTransactionList.propTypes = {
--- app/components/VTXDashboard/VTXTransactionListItem/VTXTransactionListItem.js
+++ app/components/VTXDashboard/VTXTransactionListItem/VTXTransactionListItem.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ListItem from 'components/ListItem';
+import { Col, Row } from 'smooth-ui';
+import readableTokenValue from 'utils/readableTokenValue';
+import './styles.scss'
 
 const VTXTransactionListItem = (props) => {
   const { item } = props;
+  const tokenValue = readableTokenValue(item.args.value, 18);
   const content = (
-    <div>
-      transaction: {item.transactionHash}
+    <div className="transaction-list-item">
+      <Row>
+        <Col xs={2}>
+          tx hash:
+        </Col>
+
+        <Col>
+          {item.transactionHash}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={2}>
+          from:
+        </Col>
+
+        <Col>
+          {item.args.from}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={2}>
+          to:
+        </Col>
+
+        <Col>
+          {item.args.to}
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={2}>
+          value:
+        </Col>
+
+        <Col>
+          {tokenValue}
+        </Col>
+      </Row>
     </div>
   );
 
